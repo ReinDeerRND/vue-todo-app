a4d1b593f465e5fdc9681e387fd32c5598784185
Elena Swiderskaya <olenyonock7@rambler.ru>
2023-11-02 13:18:16 +0300

    restore ts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# todo-app
+# todos
 
 ## Project setup
 ```
---
+++ src/components/AppTodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="todo-item todo-item--done" :class="{'todo-item--done': todo.completed}">
+        <div class="todo-item__status">
+            <i class="bi bi-check2"></i>
+        </div>
+        <span class="todo-item__text">{{ todo.text }}</span>
+        <button class="todo-item__remove-button">
+            <i class="bi bi-trash3"></i>
+        </button>
+    </li>
+</template>
+
+<script lang="ts">
+import { Todo } from '@/models/Todo';
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+    props: {
+        todo: {
+            type: Object as PropType<Todo>,
+            required: true
+
+        }
+    }
+})
+</script>
